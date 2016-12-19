97067fbe0a4654af620e1fb38857ca2903aacaf5
Gabriel Lidenor <gabrielslack@gmail.com>
2016-12-19 16:27:51 -0300

    seting isConnectedToNetwork() method to public

--- SConnection/Classes/SConnection.swift
+++ SConnection/Classes/SConnection.swift
@@ -3,7 +3,7 @@ import SystemConfiguration
 
 public class SConnection {
     
-    class func isConnectedToNetwork() -> Bool {
+    public class func isConnectedToNetwork() -> Bool {
         var zeroAddress = sockaddr_in()
         zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
         zeroAddress.sin_family = sa_family_t(AF_INET)
